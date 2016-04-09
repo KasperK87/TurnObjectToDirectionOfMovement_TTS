00958b7fe329187c347029d32b1696d6cf11f08a
KasperK87 <kkrist87@gmail.com>
2016-04-09 15:00:57 +0200

    Pythagoras implemented correctly!
    The speedTreshold has been squared to save a single call to math.sqrt every frame.

--- TurnObjectToDirectionOfMovement_TBS.lua
+++ TurnObjectToDirectionOfMovement_TBS.lua
@@ -1,13 +1,15 @@
 --[[set speed treshold for changing direction when dragging, need to spawn new object to take effect]]--
-local speedTreshold = 1
+local speedTreshold = 2
 
 --[[local values]]--
 local fixate
 local pickedup = false
+speedTreshold = math.pow(speedtreshold, 2)
 
 function onPickedUp()
     pickedup = true
-    fixate = self.getRotation()
+    
+    fixate = {x = self.getRotation()["x"], y = self.getRotation()["y"], z = self.getRotation()["z"]}
 end
 
 function onDropped()
@@ -18,11 +20,11 @@ function update()
     if pickedup == true then
         local velocity = self.getVelocity()
 
-         if math.sqrt(math.pow(velocity["x"],2)
-            , math.pow(velocity["z"],2)) > speedTreshold then 
+         if math.pow(velocity["x"],2)
+            + math.pow(velocity["z"],2) > speedTreshold then 
             local angle = math.atan2(velocity["x"], velocity["z"])
 
-            fixate = {x = fixate["x"], y = math.deg(angle), z = fixate["z"]}
+            fixate = {x = self.getRotation()["x"], y = math.deg(angle), z = self.getRotation()["z"]}
 
         
         else
